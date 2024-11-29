ddac04fe3ab3f274b79761243f5e7b2a7f6906f5
merve yalçın <merveyalcin.01@gmail.com>
2024-11-29 14:59:19 +0300

    adding view holder

--- app/src/main/java/com/phi/coderswag/adapter/CategoryAdapter.kt
+++ app/src/main/java/com/phi/coderswag/adapter/CategoryAdapter.kt
@@ -27,16 +27,32 @@ class CategoryAdapter(val context: Context, val categories: List<Category>): Bas
 
     @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val categoryView: View
+        val holder: ViewHolder
+        if(convertView == null) {
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            holder = ViewHolder()
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
 
-        val categoryView: View = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
-        val categoryImage: ImageView = categoryView.findViewById(R.id.categoryImage)
-        val categoryName: TextView = categoryView.findViewById(R.id.categoryName)
+            categoryView.tag = holder
+        } else {
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+        }
+
         val category = categories[position]
 
         val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
-        categoryImage.setImageResource(resourceId)
+        holder.categoryImage?.setImageResource(resourceId)
 
-        categoryName.text = category.title
+        holder.categoryName?.text = category.title
         return categoryView
+    }
+
+    private class ViewHolder {
+
+        var categoryImage: ImageView? = null
+        var categoryName: TextView? = null
     }
 }
