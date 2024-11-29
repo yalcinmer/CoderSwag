6831960a98c7f2ba23438665bd00255c8f91aa1a
merve yalçın <merveyalcin.01@gmail.com>
2024-11-29 13:55:13 +0300

    adding simple listview

--- app/src/main/java/com/phi/coderswag/model/Category.kt
+++ app/src/main/java/com/phi/coderswag/model/Category.kt
@@ -1,3 +1,8 @@
 package com.phi.coderswag.model
 
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+
+    override fun toString(): String {
+        return title
+    }
+}
