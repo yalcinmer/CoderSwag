b7a43672f2b972664ab677ba724946ef7e90afbc
merve yalçın <merveyalcin.01@gmail.com>
2024-11-29 16:05:22 +0300

    refactoring for recycler view

---
+++ app/src/main/java/com/phi/coderswag/adapter/CategoryRecycleAdapter.kt
@@ -0,0 +1,41 @@
+package com.phi.coderswag.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.phi.coderswag.R
+import com.phi.coderswag.model.Category
+
+class CategoryRecycleAdapter(val context: Context, val categories: List<Category>) :
+    RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): Holder {
+        val view = LayoutInflater.from(context).inflate(R.layout.category_list_item, parent, false)
+        return Holder(view)
+
+    }
+
+    override fun getItemCount(): Int {
+        return categories.count()
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bindCategory(categories[position], context)
+    }
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val categoryImage = itemView.findViewById<ImageView>(R.id.categoryImage)
+        val categoryName = itemView.findViewById<TextView>(R.id.categoryName)
+
+        fun bindCategory(category: Category, context: Context) {
+            val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+            categoryImage.setImageResource(resourceId)
+            categoryName.text = category.title
+        }
+    }
+}
--- app/src/main/java/com/phi/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/phi/coderswag/controller/MainActivity.kt
@@ -1,21 +1,19 @@
 package com.phi.coderswag.controller
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.phi.coderswag.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.phi.coderswag.adapter.CategoryAdapter
+import com.phi.coderswag.adapter.CategoryRecycleAdapter
 import com.phi.coderswag.databinding.ActivityMainBinding
-import com.phi.coderswag.model.Category
 import com.phi.coderswag.services.DataService
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: CategoryAdapter
+    private lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +21,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         binding.categoryListview.adapter = adapter
 
+        val layoutManager = LinearLayoutManager(this)
+        binding.categoryListview.layoutManager = layoutManager
+        binding.categoryListview.setHasFixedSize(true)
     }
 }
